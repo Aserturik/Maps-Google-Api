0c4ce723f047ad554fa46479f3e26e1f5d90eadb
Aserturik <alex.hernandez27@uptc.edu.co>
2023-11-19 17:26:53 -0500

    recoding arco.js

--- lineas entre marcadores/arco.js
+++ lineas entre marcadores/arco.js
@@ -1,17 +1,30 @@
 class arco{
   constructor(map, nodoA, nodoB){
+    this.map = map;
+    this.nodoA = nodoA;
+    this.nodoB = nodoB;
+    this.path = [nodoA.Marker.getPosition(), nodoB.Marker.getPosition()];
     this.poly = new google.maps.Polyline({
+      path: this.path,
       strokeColor: "#FF0000",
       strokeOpacity: 1.0,
       strokeWeight: 3,
       zIndex:2,
       map: map,
     });
-    this.nodoA = nodoA;
-    this.nodoB = nodoB;
-    this.path = [nodoA.Marker.getPosition(), nodoB.Marker.getPosition()];
     this.poly.setPath(this.path);
+    this.poly.setMap(map);
+    this.updateWeight();
+  }
+
+  updateWeight() {
+    const distance = google.maps.geometry.spherical.computeDistanceBetween(
+      this.nodoA.Marker.getPosition(),
+      this.nodoB.Marker.getPosition()
+    );
+    this.weight = distance;
   }
 }
+
 
 export default arco;
